7c7dec39e762b4cf9aad380d1a22b60642b8b6f2
DESKTOP-ND87ERP\Mariano <marianoh.rodriguez@davinci.edu.ar>
2022-07-24 20:36:37 -0300

    WIP - Agregado el Identity de sessions

--- TPFin/Program.cs
+++ TPFin/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using TPFin.Data;
+using Microsoft.AspNetCore.Identity;
+using TPFin.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<MyContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("MyContext")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MyContext>();
 
 var app = builder.Build();
 
@@ -24,6 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
