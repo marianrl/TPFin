d2668aed51bad6dcd056c64107ab19dee7196e74
Mariano Ghislanzoni <83178114+Maro-28@users.noreply.github.com>
2022-07-27 19:39:27 -0300

    Update HomeController.cs

--- TPFin/Controllers/HomeController.cs
+++ TPFin/Controllers/HomeController.cs
@@ -1,20 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TPFin.Data;
 using TPFin.Models;
 
 namespace TPFin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly MyContext _context; 
+        
+        public HomeController(MyContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+
+            var postContext = _context.post.Include(p => p.user);
+            var usuariosContext = _context.usuarios;
+
+            ViewData["Posts"] = postContext.ToList();
+            _ = usuariosContext != null ?
+                ViewData["Usuario"] = usuariosContext.ToList() :
+                ViewData["Usuario"] = Enumerable.Empty<string>();
             return View();
         }
 
@@ -28,5 +38,8 @@ namespace TPFin.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+        
+
     }
 }
